22d85a298087b8280094591450eb845b285880bb
Dylan H <dhuey2@illinois.edu>
2024-07-18 16:53:28 -0700

    ds fix pt2

--- global_script.js
+++ global_script.js
@@ -1,7 +1,6 @@
-// Sample D3.js script for creating the bar chart
-const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+const margin = { top: 10, right: 10, bottom: 30, left: 40 };
 const width = 480 - margin.left - margin.right;
-const height = 240 - margin.top - margin.bottom;
+const height = 200 - margin.top - margin.bottom;
 
 const svg = d3.select("#chart")
     .append("svg")
@@ -45,7 +44,10 @@ d3.csv("vgsales.csv").then(data => {
     // Create axes
     svg.append("g")
         .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(x));
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "rotate(-45)")
+        .style("text-anchor", "end");
 
     svg.append("g")
         .call(d3.axisLeft(y));
