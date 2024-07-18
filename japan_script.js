2f0dba0e94f761d51f0832fd07e9e41c7004432b
Dylan H <dhuey2@illinois.edu>
2024-07-17 21:28:56 -0700

    add japan page

---
+++ japan_script.js
@@ -0,0 +1,58 @@
+
+
+const margin = { top: 20, right: 30, bottom: 40, left: 50 };
+const width = 800 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
+
+const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// Load the CSV file
+d3.csv("vgsales.csv").then(data => {
+    // Parse the data
+    data.forEach(d => {
+        d.Year = +d.Year;
+        d.JP_Sales = +d.JP_Sales;
+    });
+
+    // Aggregate Japan sales by year
+    const salesByYear = d3.rollup(data, v => d3.sum(v, d => d.JP_Sales), d => d.Year);
+    const salesByYearArray = Array.from(salesByYear, ([year, sales]) => ({ year, sales })).sort((a, b) => a.year - b.year);
+
+    // Create scales
+    const x = d3.scaleBand()
+        .domain(salesByYearArray.map(d => d.year))
+        .range([0, width])
+        .padding(0.1);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(salesByYearArray, d => d.sales)])
+        .range([height, 0]);
+
+    // Create axes
+    svg.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Append bars to the SVG
+    svg.selectAll(".bar")
+        .data(salesByYearArray)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", d => x(d.year))
+        .attr("y", d => y(d.sales))
+        .attr("width", x.bandwidth())
+        .attr("height", d => height - y(d.sales))
+        .attr("fill", "steelblue");
+}).catch(error => {
+    console.error('Error loading or parsing the data:', error);
+});
+
+
